4882687c830d4c8384be1478ddcbbfbd6fbae594
mbulygin <maksym.bulygin@gmail.com>
2021-03-02 01:42:05 +0300

    removed unnecessary dependencies

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,8 @@
-global.jQuery = global.$ = require('jquery');
 require('./modules/_animate-css');
 var svg4everybody = require('svg4everybody');
-var waypoints = require('./modules/_waypoints');
 var smoothScroll = require('./modules/_smooth-scroll');
 
 $(document).ready(function () {
     svg4everybody();
-    waypoints();
     smoothScroll();
 });

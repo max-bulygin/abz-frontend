6b862e995cff7cacde1fcc85ce47a5a548b169cd
mbulygin <maksym.bulygin@gmail.com>
2021-03-05 21:22:32 +0300

    basic markup done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,5 @@
-require('./modules/_animate-css');
 var svg4everybody = require('svg4everybody');
-var smoothScroll = require('./modules/_smooth-scroll');
 
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     svg4everybody();
-    smoothScroll();
 });
